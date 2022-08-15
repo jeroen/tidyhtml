6d0e7723da2fc2a7f0fc0a9385d80818b52acd2a
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-15 16:02:52 +0200

    Add some wrappers

--- R/tidyhtml.R
+++ R/tidyhtml.R
@@ -3,14 +3,31 @@
 #' Cleans up HTML and also returns a list of warnings.
 #'
 #' @export
-#' @param file path to html file
+#' @rdname tidy_html
+#' @param file path to input file
 #' @useDynLib tidyhtml C_tidy_html
-tidy_html <- function(file){
+tidy_html_file <- function(file){
   file <- normalizePath(file, mustWork = TRUE)
   str <- rawToChar(readBin(file, raw(), file.info(file)$size))
-  out <- .Call(C_tidy_html, str, basename(file))
-  list(
-    diagnostics = out[1],
-    html = out[2]
-  )
+  tidy_html_string(str, basename(file))
+}
+
+#' @export
+#' @rdname tidy_html
+#' @param filename (dummy) name to show in output
+tidy_html_string <- function(str, filename = ""){
+  out <- .Call(C_tidy_html, str, filename)
+  structure(as.list(out), names = c('diagnostics', 'html'))
+}
+
+#' @export
+#' @rdname tidy_html
+#' @param filename (dummy) name to show in output
+#' @param ... passed to `tools::Rd2HTML`
+tidy_html_rd <- function(file, ...){
+  file <- normalizePath(file, mustWork = TRUE)
+  out <- file.path(tempdir(), paste0(basename(file), '.html'))
+  on.exit(unlink(out))
+  tools::Rd2HTML(file, out = out, ...)
+  tidy_html_file(out)
 }
